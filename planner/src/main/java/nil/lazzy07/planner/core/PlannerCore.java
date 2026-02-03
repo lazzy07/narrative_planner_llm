0fa6165251fa628bb1313334fadf5c85329b34de
Lasantha Senanayake <222lasantha@gmail.com>
2026-02-02 22:07:39 -0500

    Planner core functionality added

---
+++ planner/src/main/java/nil/lazzy07/planner/core/PlannerCore.java
@@ -0,0 +1,21 @@
+/*
+* File name: PlannerCore.java
+* Project: 
+* Author: Lasantha M Senanayake
+* Date created: 2026-02-02 22:01:17
+// Date modified: 2026-02-02 22:05:41
+* ------
+*/
+
+package nil.lazzy07.planner.core;
+
+import nil.lazzy07.planner.config.ConfigFile;
+
+public class PlannerCore {
+  ConfigFile configs;
+
+  public PlannerCore(ConfigFile configs) {
+    this.configs = configs;
+  }
+
+}
