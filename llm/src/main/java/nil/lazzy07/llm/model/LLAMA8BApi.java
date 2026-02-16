fda4b5a2617b7d8e371c6bd841d5ac715f01d909
Lasantha Senanayake <222lasantha@gmail.com>
2026-02-16 01:52:41 -0500

    LLMApi cahnges

--- llm/src/main/java/nil/lazzy07/llm/model/LLAMA8BApi.java
+++ llm/src/main/java/nil/lazzy07/llm/model/LLAMA8BApi.java
@@ -3,11 +3,16 @@
 * Project: 
 * Author: Lasantha M Senanayake
 * Date created: 2026-02-11 03:09:39
-// Date modified: 2026-02-11 03:20:23
+// Date modified: 2026-02-15 17:07:45
 * ------
 */
 
 package nil.lazzy07.llm.model;
 
+import nil.lazzy07.domain.converters.DomainConverter;
+
 public class LLAMA8BApi extends LLMApi {
+  public LLAMA8BApi() {
+    super("LLAMA-8B");
+  }
 }
--- llm/src/main/java/nil/lazzy07/llm/model/LLMApi.java
+++ llm/src/main/java/nil/lazzy07/llm/model/LLMApi.java
@@ -3,21 +3,24 @@
 * Project: 
 * Author: Lasantha M Senanayake
 * Date created: 2026-02-11 03:09:57
-// Date modified: 2026-02-11 03:23:01
+// Date modified: 2026-02-15 17:08:49
 * ------
 */
 
 package nil.lazzy07.llm.model;
 
+import nil.lazzy07.domain.converters.DomainConverter;
+
 public abstract class LLMApi {
   private String type;
+  private DomainConverter domainConverter;
 
   public LLMApi(String type) {
     this.type = type;
   }
 
-  public void init() {
-
+  public void init(DomainConverter domainConverter) {
+    this.domainConverter = domainConverter;
   }
 
   public String getType() {
