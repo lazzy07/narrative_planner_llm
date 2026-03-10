92d5ff4c9a023ecc097220d4f61a9854ab6b2852
Lasantha Senanayake <222lasantha@gmail.com>
2026-03-09 21:48:23 -0400

    LLMApiFactory changes

--- llm/src/main/java/nil/lazzy07/llm/LLMApiFactory.java
+++ llm/src/main/java/nil/lazzy07/llm/LLMApiFactory.java
@@ -3,23 +3,23 @@
 * Project: 
 * Author: Lasantha M Senanayake
 * Date created: 2026-01-25 22:44:36
-// Date modified: 2026-03-06 17:58:38
+// Date modified: 2026-03-09 16:30:58
 * ------
 */
 
 package nil.lazzy07.llm;
 
-import nil.lazzy07.llm.model.ChatGPT5MiniSelectStructured;
-import nil.lazzy07.llm.model.LLAMA8BSelectImproved;
+import nil.lazzy07.llm.model.ChatGPTMiniJson;
+import nil.lazzy07.llm.model.LLAMA8BJson;
 import nil.lazzy07.llm.model.LLMApi;
 
 public class LLMApiFactory {
   public static LLMApi GetLLMApi(String type, boolean useCache, String cacheDirectory, String domain) {
     switch (type) {
       case "chatgpt-5-mini":
-        return new ChatGPT5MiniSelectStructured(useCache, cacheDirectory, domain);
+        return new ChatGPTMiniJson(useCache, cacheDirectory, domain);
       case "llama-8b":
-        return new LLAMA8BSelectImproved(useCache, cacheDirectory, domain);
+        return new LLAMA8BJson(useCache, cacheDirectory, domain);
       default:
         return null;
     }
