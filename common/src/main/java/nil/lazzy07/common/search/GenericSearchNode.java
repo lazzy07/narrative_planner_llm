793224b109d149d73f5a80b728a7c21e8f5fdcc1
Lasantha Senanayake <222lasantha@gmail.com>
2026-03-23 21:57:41 -0400

    Current action functionality added to the search node

--- common/src/main/java/nil/lazzy07/common/search/GenericSearchNode.java
+++ common/src/main/java/nil/lazzy07/common/search/GenericSearchNode.java
@@ -3,13 +3,15 @@
 * Project: 
 * Author: Lasantha M Senanayake
 * Date created: 2026-02-10 23:49:26
-// Date modified: 2026-03-06 15:12:51
+// Date modified: 2026-03-11 20:52:27
 * ------
 */
 
 package nil.lazzy07.common.search;
 
 import java.util.List;
+
+import edu.uky.cs.nil.sabre.comp.CompiledAction;
 
 public interface GenericSearchNode {
   public long getNodeId();
@@ -23,4 +25,6 @@ public interface GenericSearchNode {
   public void addChildNode(GenericSearchNode node);
 
   public String getExplaination();
+
+  public CompiledAction getCurrentAction();
 }
